a71134af398de5d2da78654ba253ce304159a41a
maiferna <maiderfernandez57@gmail.com>
2025-05-22 11:02:48 +0200

    ADD: pseudocódigo

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+/**
+ * Llamada a los elementos del DOM
+ */
+
+const infoUsuario = document.querySelector('#infoUsuario');
+
+
+
+/**
+ * Evento
+ */
+
+document.addEventListener("click", (ev) => {
+    if (ev.target.matches('#btnDatos')) {
+        // crear promesa
+    }
+})
+
+
+/**
+ * Funciones
+ */
+
+
+const api = () => {
+    const promise = new Promise((resolve, reject) => {
+        // setTiemout 2 segundos
+        // Variable true o false
+        // si es true resolver --> crear objeto
+        // si es false reject --> error
+    })
+}
+
+
+const getApi = () => {
+    // Llamar a api
+    // .then() --> pintarlista
+    // .catch --> error
+}
+
+
+const drawList = () => {
+    // pintar lista
+ }
